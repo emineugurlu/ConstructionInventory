b7abdbfc2cfb013f309bfe8b6c53fbf1fdb396c5
Emine Ugurlu <emineugurlu957@gmail.com>
2026-01-24 12:54:13 +0300

    database

---
+++ ConstructionInventory.API/Controllers/MaterialsController.cs
@@ -0,0 +1,26 @@
+﻿using ConstructionInventory.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConstructionInventory.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public MaterialsController(AppDbContext context)
+        {
+           _context = context;
+        }
+        //malzeme listeleme
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var materials = await _context.Materials.ToListAsync();
+            return Ok(materials);
+        }
+    }
+}
--- ConstructionInventory.API/Program.cs
+++ ConstructionInventory.API/Program.cs
@@ -1,12 +1,33 @@
 using ConstructionInventory.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Servisleri Kaydet
+builder.Services.AddControllers(); // Bu satýr Controller'larýný sisteme tanýtýr!
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); // Swagger (test ekraný) için gerekli
 
 // Veritabaný baðlantýsý
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
-app.MapGet("/", () => "Sistem Hazýr!");
-app.Run(); ;      
+
+// 2. HTTP Ýstek Hattýný Yapýlandýr
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); // Tarayýcýda /swagger yazýnca açýlan ekran
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+// 3. Rotalarý Eþleþtir
+app.MapControllers(); // Adres çubuðuna yazýlanlarý Controller'lara yönlendirir
+
+app.MapGet("/", () => "Sistem Hazýr! Test için /api/materials veya /swagger adresine gidin.");
+
+app.Run();

8a87f3591c2969815a29204b6c340de43386d2b3
Emine Ugurlu <emineugurlu957@gmail.com>
2026-01-24 15:27:20 +0300

    .

---
+++ ConstructionInventory.API/Controllers/StockMovementsController.cs
@@ -0,0 +1,18 @@
+﻿using ConstructionInventory.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionInventory.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockMovementsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StockMovementsController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}

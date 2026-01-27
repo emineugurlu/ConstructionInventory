762c35cbd083abaa58f1b565faae332b972322cc
Emine Ugurlu <emineugurlu957@gmail.com>
2026-01-27 17:42:09 +0300

    .

---
+++ ConstructionInventory.Domain/Services/IMaterialService.cs
@@ -0,0 +1,17 @@
+﻿using ConstructionInventory.Domain.DTOs;
+using ConstructionInventory.Domain.Entities;
+using ConstructionInventory.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConstructionInventory.Domain.Services
+{
+    public interface IMaterialService
+    {
+        Task<List<Material>> GetAllMaterialsAsync();
+        Task<StockDashboardDto> GetDashboardStatsAsync();
+        Task<bool> MoveStockAsync(int materialId, decimal quantity, MovementType type);
+    }
+}
